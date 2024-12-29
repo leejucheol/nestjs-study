80e45624784141ffdb69ba42144d4856e9786576
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-29 18:02:08 +0900

    study epress

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-
+// express 공부
 const app = express(); // app이 서버 역할을 한다 (변수명?)
 const port = 3000;
 app.get("/test", (req: express.Request, res: express.Response) => {

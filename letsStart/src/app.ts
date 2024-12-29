0836a6f37c7eb88f96b896cd8d5668ca36db819b
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-29 19:58:08 +0900

    add: json middleware and create api

--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,12 +1,83 @@
 import * as express from "express";
-// express 공부
-const app = express(); // app이 서버 역할을 한다 (변수명?)
-const port = 3000;
-app.get("/test", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send("Hello World!@@");
+import { Cat } from "./app.modelcat";
+
+const app: express.Express = express();
+
+//* logging middleware
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+//* json middleware / express에서 제공하는 middleware를 사용
+app.use(express.json());
+
+//* READ 고양이 전체 데이터 다 조회 (회원 정보라고 생각해도됨) CRUD가 중요
+app.get("/cats", (req, res) => {
+  try {
+    const cats = Cat;
+    throw new Error("db connect error");
+    res.status(200).send({
+      success: true,
+      data: {
+        cats,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* READ 특정 고양이 데이터 조회 (유저라고 하면 본인 혹은 특정 유저 조회)
+app.get("/cats/:id", (req, res) => {
+  try {
+    const params = req.params;
+    console.log(params);
+    const cat = Cat.find((cat) => {
+      return cat.id === params.id;
+    });
+    res.status(200).send({
+      success: true,
+      data: {
+        cat,
+      },
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* CREATE 새로운 고양이 추가 api (유저가 회원가입 혹은 블로그 포스팅하는 거랑 비슷)
+app.post("/cats", (req, res) => {
+  try {
+    const data = req.body;
+    console.log(data);
+    Cat.push(data); // data를 입력하면 create가 됨
+    res.status(200).send({
+      success: true,
+      data: data,
+    });
+  } catch (error: any) {
+    res.status(400).send({
+      success: false,
+      error: error.message,
+    });
+  }
+});
+
+//* 404 middleware
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on...");
 });

6140128b40cc89db24badc202ccfe3a30cf1cb03
leejucheol <161808249+leejucheol@users.noreply.github.com>
2024-12-29 18:42:50 +0900

    study express middleware

---
+++ letsStart/src/app.model.ts
@@ -0,0 +1,19 @@
+export type CatType = {
+  id: string;
+  name: string;
+  age: number;
+  species: string;
+  isCute: boolean;
+  friends: string[];
+};
+
+export const Cat: CatType[] = [
+  {
+    id: "fsduifh",
+    name: "blue",
+    age: 8,
+    species: "Russian Blue",
+    isCute: true,
+    friends: ["asdfhj29009", "WE09tju2j"],
+  },
+];
--- letsStart/src/app.ts
+++ letsStart/src/app.ts
@@ -1,12 +1,34 @@
+import { error } from "console";
+import { Cat } from "./app.model";
 import * as express from "express";
-// express 공부
-const app = express(); // app이 서버 역할을 한다 (변수명?)
-const port = 3000;
-app.get("/test", (req: express.Request, res: express.Response) => {
-  console.log(req);
-  res.send("Hello World!@@");
+
+const app: express.Express = express();
+
+// app.use는 middleware라고 함. / app.get 라우터 부분에 next라는 인자만 추가해주면 middleware가 됨 ex) app.get("/cats/blue", (req, res, next)
+app.use((req, res, next) => {
+  console.log(req.rawHeaders[1]);
+  console.log("this is logging middleware");
+  next();
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+// const data = [1, 2, 3, 4];
+
+// app.get들은 다 라우터라고 함.
+app.get("/", (req: express.Request, res: express.Response) => {
+  //console.log(req.rawHeaders[1]);
+  res.send({ cats: Cat });
+});
+
+app.get("/cats/blue", (req, res) => {
+  //console.log(req.rawHeaders[1]);
+  res.send({ blue: Cat[0] });
+});
+
+app.use((req, res, next) => {
+  console.log("this is error middleware");
+  res.send({ error: "404 not found error" });
+});
+
+app.listen(8000, () => {
+  console.log("server is on...");
 });
